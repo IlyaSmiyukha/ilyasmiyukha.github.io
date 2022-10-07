10c493dce55593410ee4480dfca56a310bba656a
ilya Smiyukha <ilya.smiyukha@touchcast.com>
2022-10-07 11:10:05 +0300

    change project text

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import Content from 'components/Content';
 
+const widthAnimation = keyframes`
+ 0% {  width: 100px; }
+ 100% { width: 110px; }
+`
 const P = styled.p`
   font-size: 1.25rem;
-  max-width: 150px;
   line-height: 2rem;
+  width: 110px;
+`;
+
+const Animation = styled.div`
+  overflow: hidden;
+  white-space: nowrap;
+  animation: ${widthAnimation} 3s infinite ease-in-out;
 `;
 
 const  Projects = () => {
   return (
     <Content>
       <P>
-        Projects
+        <Animation>
+          Coming soon...
+        </Animation>
       </P>
     </Content>
   );
