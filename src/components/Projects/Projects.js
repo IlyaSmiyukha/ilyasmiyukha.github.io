e18c0711d6d7a0ffe704ab4955f388a02d5f4a0e
ilya Smiyukha <ilya.smiyukha@touchcast.com>
2022-10-22 19:28:59 +0300

    add progect

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,27 +7,29 @@ const widthAnimation = keyframes`
  0% {  width: 100px; }
  100% { width: 110px; }
 `
-const P = styled.p`
-  font-size: 1.25rem;
+
+const A = styled.a`
+  font-size: 2.5rem;
   line-height: 2rem;
   width: 110px;
+  color:  ${props => props.theme.colors.text};
+  font-weight: 300;
+  text-decoration: none;
+  letter-spacing: -1px;
+  word-spacing: -1px;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
-const Animation = styled.span`
-  display: block;
-  overflow: hidden;
-  white-space: nowrap;
-  animation: ${widthAnimation} 3s infinite ease-in-out;
-`;
 
 const  Projects = () => {
   return (
     <Content>
-      <P>
-        <Animation>
-          Coming soon...
-        </Animation>
-      </P>
+      <A href='https://ilyasmiyukha.github.io/by-me-a-coffee/' target="_blank" rel="noopener norefferer">
+        Buy me a crypto coffee
+      </A>
     </Content>
   );
 }
